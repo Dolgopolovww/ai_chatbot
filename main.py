a61ff608857916a8675a9f5d2a6ba72d2b75e7dd
Andrey <Dolgopolov.ww@gmail.com>
2021-02-15 15:40:19 +0700

    new import

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-# основной
+import numpy as np
+from keras.datasets import mnist
+from keras import models, layers
+from keras.utils import to_categorical
+import matplotlib.pyplot as plt
+import os
+from tensorflow import keras
+from tensorflow.keras.layers import Dense, Flatten
+from tensorflow.keras.datasets import mnist
+
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2" # для исклчение ошибки
